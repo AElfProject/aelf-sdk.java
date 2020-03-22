0dddea389423cfb174a0c1c037869d086064f57e
linhui <linhui2@tydic.com>
2020-03-23 02:29:38 +0800

    add msg

---
+++ AElfClient/src/main/java/io/aelf/utils/TransactionResultDtoExtension.java
@@ -0,0 +1,11 @@
+package io.aelf.utils;
+
+/**
+ * @author linhui linhui@tydic.com
+ * @title: TransactionResultDtoExtension
+ * @description: TODO
+ * @date 2020/3/230:40
+ */
+public class TransactionResultDtoExtension {
+
+}
