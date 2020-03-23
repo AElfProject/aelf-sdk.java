e0ffee58b49ea2cb817d99ed79d7fa906e9357a8
linhui <linhui2@tydic.com>
2020-03-24 01:10:12 +0800

    add msg

--- AElfClient/src/main/java/io/aelf/utils/TransactionResultDtoExtension.java
+++ AElfClient/src/main/java/io/aelf/utils/TransactionResultDtoExtension.java
@@ -1,6 +1,7 @@
 package io.aelf.utils;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
+import io.aelf.protobuf.generated.TransactionFee.ResourceTokenCharged;
 import io.aelf.protobuf.generated.TransactionFee.TransactionFeeCharged;
 import io.aelf.schemas.LogEventDto;
 import io.aelf.schemas.TransactionResultDto;
@@ -16,14 +17,16 @@ import org.apache.commons.codec.binary.Base64;
  */
 public class TransactionResultDtoExtension {
 
-  public static HashMap<String,Long> GetTransactionFees(TransactionResultDto transactionResultDto)
+  public static HashMap<String,Long> getTransactionFees(TransactionResultDto transactionResultDto)
       throws InvalidProtocolBufferException {
     HashMap transactionFeesDict = new HashMap<String,Long>();
+    if(transactionResultDto==null || transactionResultDto.getLogs()==null){
+      return transactionFeesDict;
+    }
     List<LogEventDto> eventLogs = transactionResultDto.getLogs();
-    if (eventLogs == null) return transactionFeesDict;
     for (LogEventDto log:eventLogs)
     {
-      if (StringUtil.toString(log.getName()).contains("ResourceTokenCharged") || StringUtil.toString(log.getName()).contains("TransactionFeeCharged"))
+      if (StringUtil.toString(log.getName()).contains("TransactionFeeCharged"))
       {
         Base64 base64 = new Base64();
         byte[] byteStringMessage=base64.decode(log.getNonIndexed());
@@ -31,6 +34,18 @@ public class TransactionResultDtoExtension {
         transactionFeesDict.put(info.getSymbol(), info.getAmount());
       }
     }
+
+    for (LogEventDto log:eventLogs)
+    {
+      if (StringUtil.toString(log.getName()).contains("ResourceTokenCharged"))
+      {
+        Base64 base64 = new Base64();
+        byte[] byteStringMessage=base64.decode(log.getNonIndexed());
+        ResourceTokenCharged info = ResourceTokenCharged.parseFrom(ByteString.copyFrom(byteStringMessage));
+        transactionFeesDict.put(info.getSymbol(), info.getAmount());
+      }
+    }
+
 
     return transactionFeesDict;
   }
